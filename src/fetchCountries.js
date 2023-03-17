7f823d1b2671833169edd70ebcdd8f3f12a586fc
NataliTs <natikz20@ukr.net>
2023-03-17 18:18:05 +0200

    add hendle error 404

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,9 +2,12 @@ export const getData = name => {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,flags,capital,population,languages`
   ).then(res => {
-    if (res.ok) {
-      return res.json();
+    if (!res.ok) {
+      if (res.status === 404) {
+        return [];
+      }
+      throw new Error(res.status);
     }
-    throw new Error('Failed to fetch');
+    return res.json();
   });
 };
